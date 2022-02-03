03ca513a79cbf265f71bca6343072a631526815f
EmaleeSoto <emaleesoto@gmail.com>
2022-02-03 10:32:13 -0500

    completed first function

--- index.js
+++ index.js
@@ -26,7 +26,13 @@ const animals = [
  * getTotalCount(animals); //> 28
  * getTotalCount([]); //> 0 // returns 0 if the input array is empty
  */
-function getTotalCount(animals) {}
+function getTotalCount(animals) {
+  let sum = 0;
+  for (const animal of animals) {
+    sum += animal.count;
+  }
+  return sum;
+}
 
 /**
  * FUNCTION DESCRIPTION
